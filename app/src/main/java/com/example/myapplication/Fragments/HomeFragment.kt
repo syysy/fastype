f469ac4569b4e8e310ade0bf66eb1fb96cb8d4d5
syysy <110264030+syysy@users.noreply.github.com>
2022-08-05 00:50:47 +0200

    Layouts

--- app/src/main/java/com/example/myapplication/Fragments/HomeFragment.kt
+++ app/src/main/java/com/example/myapplication/Fragments/HomeFragment.kt
@@ -14,7 +14,7 @@ import kotlin.random.Random
 class HomeFragment: Fragment(){
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_home, container,false)
+        return inflater.inflate(R.layout.register, container,false)
     }
 
 }
