162ee5e40f237f72f88b8bda1c8e8f361f9c2b46
Mathieu Marthy <l4p0ubelle@gmail.com>
2022-08-05 18:56:56 +0200

    1.2

---
+++ app/src/main/java/com/example/myapplication/Fragments/RegisterFragment.kt
@@ -0,0 +1,20 @@
+package com.example.myapplication.Fragments
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.myapplication.R
+import java.io.File
+import java.util.*
+import kotlin.random.Random
+
+class RegisterFragment: Fragment(){
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.register, container,false)
+    }
+
+}
