718d4a417531a0dacde109de4113325ef358e60c
E216351P <Ethan27349570!>
2022-09-01 18:53:20 +0200

    edit profil -> all edit things done

--- app/src/main/java/com/example/myapplication/EditProfilActivity.kt
+++ app/src/main/java/com/example/myapplication/EditProfilActivity.kt
@@ -6,6 +6,8 @@ import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
+import android.text.Editable
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.MenuItem
@@ -14,6 +16,7 @@ import android.view.ViewGroup
 import android.widget.*
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.net.toUri
 import com.bumptech.glide.Glide
 import com.example.myapplication.BaseDeDonnées.StatsRepository
 import com.example.myapplication.databinding.EditProfilBinding
@@ -21,13 +24,19 @@ import com.example.myapplication.databinding.HeaderLayoutBinding
 import com.example.myapplication.objets.ProfilModel
 import com.google.android.gms.ads.AdRequest
 import com.google.android.gms.ads.AdView
+import com.google.android.gms.tasks.Continuation
+import com.google.android.gms.tasks.Task
 import com.google.android.material.textfield.TextInputEditText
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
+import com.google.firebase.storage.UploadTask
 import org.json.JSONException
 import org.json.JSONObject
 import java.io.IOException
+import java.net.URI
 import java.nio.charset.Charset
 import java.util.*
 
@@ -36,16 +45,19 @@ class EditProfilActivity : AppCompatActivity() {
     lateinit var binding: EditProfilBinding
     private lateinit var firebaseAuth: FirebaseAuth
     private lateinit var databaseRef : DatabaseReference
+    private lateinit var storageRef : StorageReference
     private lateinit var toggle : ActionBarDrawerToggle
     private lateinit var headerLayout : HeaderLayoutBinding
     private lateinit var userModel : ProfilModel
+    private var file : Uri? = null
+    // header changements
 
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = EditProfilBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        // header changements
+
         val inflater: LayoutInflater = this@EditProfilActivity.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
         val viewGroup : ViewGroup = findViewById (R.id.nav_view)
         val view = inflater.inflate(R.layout.header_layout, viewGroup)
@@ -53,11 +65,11 @@ class EditProfilActivity : AppCompatActivity() {
         val email : TextView = view.findViewById(R.id.text_user_mail)
         val image : ImageView = view.findViewById(R.id.image_user)
         headerLayout = HeaderLayoutBinding.inflate(layoutInflater)
-
         // Changement de l'image du profil
         userModel = ProfilModel("","",0.0,0,"")
         firebaseAuth = FirebaseAuth.getInstance()
         databaseRef = FirebaseDatabase.getInstance().getReference("players")
+        storageRef = FirebaseStorage.getInstance().getReferenceFromUrl("gs://fastype-c5cc4.appspot.com")
 
         val userDate = firebaseAuth.currentUser
         val date = Date(userDate!!.metadata!!.creationTimestamp)
@@ -76,7 +88,7 @@ class EditProfilActivity : AppCompatActivity() {
 
         databaseRef.child(firebaseAuth.currentUser!!.uid).child("name").get().addOnSuccessListener {
             userModel.name = it.value.toString()
-            textPseudo.hint = it.value.toString()
+            textPseudo.setText(it.value.toString())
             name.text = it.value.toString()
         }
         databaseRef.child(firebaseAuth.currentUser!!.uid).child("email").get().addOnSuccessListener {
@@ -110,7 +122,6 @@ class EditProfilActivity : AppCompatActivity() {
 
         spinnerCountry.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
             override fun onItemSelected(parent: AdapterView<*>, view: View, position: Int, id: Long) {
-                userModel.country = spinnerCountry.selectedItem.toString()
                 try {
                     if (arrayList[position] == "Unknown"){
                         Glide.with(binding.root).load(Uri.parse(obj[arrayList[position]].toString())).into(imageCountry)
@@ -174,6 +185,34 @@ class EditProfilActivity : AppCompatActivity() {
 
         binding.imageProfil.setOnClickListener { pickImage() }
 
+        binding.Save.setOnClickListener {
+            if (file != null  && file.toString() != userModel.imageAvatarUrl) {
+                uploadImage(file!!){
+
+                    val newCountry = spinnerCountry.selectedItem.toString()
+                    val country = userModel.country
+                    if (textPseudo.text.toString() != userModel.name){
+                        databaseRef.child(firebaseAuth.currentUser!!.uid).child("name").setValue(textPseudo.text.toString())
+                        userModel.name = textPseudo.text.toString()
+                    }
+                    if (newCountry != userModel.country){
+                        databaseRef.child(firebaseAuth.currentUser!!.uid).child("country").setValue(newCountry)
+                        userModel.country = newCountry
+                    }
+                    startActivity(Intent(this,ProfilActivity::class.java))
+                }
+            }else{
+                if (textPseudo.text.toString() != userModel.name){
+                    databaseRef.child(firebaseAuth.currentUser!!.uid).child("name").setValue(textPseudo.text.toString())
+                    userModel.name = textPseudo.text.toString()
+                }
+                if (spinnerCountry.selectedItem.toString() != userModel.country){
+                    databaseRef.child(firebaseAuth.currentUser!!.uid).child("country").setValue(spinnerCountry.selectedItem.toString())
+                }
+                startActivity(Intent(this,ProfilActivity::class.java))
+            }
+        }
+
 
     }
 
@@ -183,14 +222,27 @@ class EditProfilActivity : AppCompatActivity() {
         intent.action = Intent.ACTION_GET_CONTENT
         startActivityForResult(Intent.createChooser(intent, "Select Picture"), 11)
     }
+    private fun uploadImage(file : Uri,callback : () -> Unit = {}) {
+        val ref = storageRef.child(firebaseAuth.currentUser!!.uid)
+        val uploadTask = ref.putFile(file)
+        uploadTask.addOnFailureListener {
+            Toast.makeText(this, "Upload failed", Toast.LENGTH_SHORT).show()
+        }.addOnSuccessListener {
+            Toast.makeText(this, "Upload successful", Toast.LENGTH_SHORT).show()
+            ref.downloadUrl.addOnSuccessListener {
+                val uploadedImage = it.toString()
+                databaseRef.child(firebaseAuth.currentUser!!.uid).child("imageAvatarUrl").setValue(uploadedImage)
+                callback()
+            }
+        }
+    }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if (requestCode == 11 && resultCode == Activity.RESULT_OK && data != null && data.data != null) {
-            val uri = data.data
-            println("uri : $uri")
-            binding.imageProfil.setImageURI(uri)
-            databaseRef.child(firebaseAuth.currentUser!!.uid).child("imageAvatarUrl").setValue(uri.toString())
+            file = data.data!!
+            binding.imageProfil.setImageURI(file)
+            Glide.with(binding.root).load(Uri.parse(file.toString())).into(binding.imageProfil)
         }
     }
 
