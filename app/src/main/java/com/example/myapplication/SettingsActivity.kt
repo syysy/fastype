3cef547e27d6bb6fc48877b40634cd80b34ccdab
Mathieu Marthy <l4p0ubelle@gmail.com>
2022-09-05 22:15:18 +0200

    listern spinner languages

--- app/src/main/java/com/example/myapplication/SettingsActivity.kt
+++ app/src/main/java/com/example/myapplication/SettingsActivity.kt
@@ -1,18 +1,29 @@
 package com.example.myapplication
 
 import android.R
+import android.app.LocaleManager
 import android.content.Intent
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.os.LocaleList
+import android.view.View
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
+import com.bumptech.glide.Glide
 import com.example.myapplication.databinding.SettingsBinding
+import org.json.JSONException
+import java.util.*
 
 class SettingsActivity : AppCompatActivity() {
 
     private lateinit var binding: SettingsBinding
     private lateinit var toggle: ActionBarDrawerToggle
+    private val languages = arrayListOf<String>("Français", "English")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,7 +41,6 @@ class SettingsActivity : AppCompatActivity() {
             resetPassword()
         }
 
-        val languages = arrayListOf<String>("Français", "English")
 
         val arrayAdapter: ArrayAdapter<String> =
             ArrayAdapter<String>(this, R.layout.simple_spinner_item, languages)
@@ -38,6 +48,20 @@ class SettingsActivity : AppCompatActivity() {
         arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         binding.spinnerLanguages.setAdapter(arrayAdapter)
         binding.spinnerLanguages.setSelection(0)
+
+        binding.spinnerLanguages.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>, view: View, position: Int, id: Long) {
+                val language = parent.getItemAtPosition(position).toString()
+                if (language == "Français") {
+                    TODO("francais")
+                } else {
+                    TODO("anglais")
+                }
+            }
+            override fun onNothingSelected(parent: AdapterView<*>) {
+                // Another interface callback
+            }
+        }
     }
 
     override fun onStart() {
@@ -47,4 +71,5 @@ class SettingsActivity : AppCompatActivity() {
     fun resetPassword() {
         startActivity(Intent(this, ForgotPasswordActivity::class.java))
     }
+
 }
